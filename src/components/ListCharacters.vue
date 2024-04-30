9a7f2a75cab3a3f753a78026044aaf7003fbbfab
PedroNalom <128000616+PedroNalom@users.noreply.github.com>
2024-04-29 21:36:46 -0300

    Update ListCharacters.vue

--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -4,14 +4,30 @@ const characterId = character.url.split('/')[5]
 const imageUrl= "https://rickandmortyapi.com/api/character/avatar/" + characterId + ".jpeg"
 console.log(imageUrl)
 
+let coloar = 'gree'
+
+switch(character.status){
+    case "Alive":
+        coloar = "green";
+        break;
+    case "Dead":
+        coloar = "red";
+        break;
+    case "unknown":
+        coloar = "blue";
+        break;
+    default:
+        coloar = "black";
+}
+
 </script>
 
 <template>  
-    <div class="col-md-4">
-        <div class="card p-2 mb-3">
-            <img :src="imageUrl" height="250" alt="">
+    <div class="col-md-3">  
+        <div class="card p-2 mb-3" style="height: 35rem">
+            <img :src="imageUrl" height="250rem" alt="" style="margin: auto;">
             <p class="text-center">Nome: {{character.name}}</p>
-            <p class="text-center">Status: {{character.status}}</p>
+            <p class="text-center" :style="{color: coloar}">Status: {{character.status}}</p>
             <p class="text-center">Espécie: {{character.species}}</p>
             <p class="text-center">Gênero: {{character.gender}}</p>
             <p class="text-center">Localização: {{character.location.name}}</p>
